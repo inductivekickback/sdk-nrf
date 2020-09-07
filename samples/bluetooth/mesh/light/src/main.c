bf7c9e4eed2421b3b9af200ab8dc32b7cb67e014
Daniel Veilleux <inductivekickback@gmail.com>
2020-09-07 16:28:28 -0700

    Add USB_DETECT pin interrupt callback

--- samples/bluetooth/mesh/light/src/main.c
+++ samples/bluetooth/mesh/light/src/main.c
@@ -7,11 +7,26 @@
 /** @file
  *  @brief Nordic Mesh light sample
  */
+#include <device.h>
+#include <drivers/gpio.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/mesh/models.h>
 #include <bluetooth/mesh/dk_prov.h>
 #include <dk_buttons_and_leds.h>
 #include "model_handler.h"
+
+struct gpio_pin {
+	const char * const port;
+	const u8_t number;
+};
+
+static const struct gpio_pin usb_detect_pin = {
+	DT_GPIO_LABEL(DT_NODELABEL(usbdetect), gpios),
+	DT_GPIO_PIN(DT_NODELABEL(usbdetect), gpios)
+};
+
+static struct device *usb_detect_button;
+static struct gpio_callback button_cb_data;
 
 static void bt_ready(int err)
 {
@@ -41,11 +56,54 @@ static void bt_ready(int err)
 	printk("Mesh initialized\n");
 }
 
+static void button_pressed(struct device *dev, struct gpio_callback *cb, u32_t pins)
+{
+    int val;
+
+    val = gpio_pin_get_raw(usb_detect_button, usb_detect_pin.number);
+    if (val) {
+        printk("Button high");
+    } else {
+        printk("Button low");
+    }
+}
+
+static int button_init(void)
+{
+	int ret;
+
+	usb_detect_button = device_get_binding(usb_detect_pin.port);
+	if (!usb_detect_button) {
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure(usb_detect_button, usb_detect_pin.number, GPIO_INPUT);
+	if (ret != 0) {
+		return ret;
+	}
+
+	ret = gpio_pin_interrupt_configure(usb_detect_button,
+                                       usb_detect_pin.number,
+                                       GPIO_INT_EDGE_BOTH);
+	if (ret != 0) {
+		return ret;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(usb_detect_pin.number));
+	gpio_add_callback(usb_detect_button, &button_cb_data);
+    return 0;
+}
+
 void main(void)
 {
 	int err;
 
 	printk("Initializing...\n");
+
+    err = button_init();
+    if (err) {
+        printk("Failed to initialize buttons");
+    }
 
 	err = bt_enable(bt_ready);
 	if (err) {
