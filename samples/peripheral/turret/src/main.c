f596d3794353833a68a7fb156674410ae695f792
Daniel Veilleux <inductivekickback@gmail.com>
2020-12-17 19:48:42 -0800

    Cleaned up a bunch of stuff

--- samples/peripheral/turret/src/main.c
+++ samples/peripheral/turret/src/main.c
@@ -13,6 +13,43 @@
 #define LOG_MODULE_NAME app
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
+#define PWM_PIN 31
+
+/*
+ * With an 8MHz clock the 37.9KHz period is ~211 ticks.
+ */
+#define TICKS_PER_PERIOD 		211
+#define DUTY_CYCLE_0     		TICKS_PER_PERIOD
+#define DUTY_CYCLE_50    		(TICKS_PER_PERIOD / 2)
+#define REFRESH_COUNT_400US    	14
+
+
+/*
+ * BLUE pistol, shotgun, rocket.
+ * 0  0  0  0_ 0  0  0  0_ 0  0  0 _1 
+ * 0  0  0  0_ 0  0  0  1  0  0  0 _1_
+ * 0  0  0  0_ 0  0  0  1_ 0  0  1  0 
+ */
+#define PREAMBLE DUTY_CYCLE_50,DUTY_CYCLE_50,DUTY_CYCLE_50,DUTY_CYCLE_50
+#define SH       DUTY_CYCLE_50
+#define LH       DUTY_CYCLE_50,DUTY_CYCLE_50
+#define SL       DUTY_CYCLE_0
+#define LL       DUTY_CYCLE_0,DUTY_CYCLE_0
+#define PREFIX   PREAMBLE,SL,SH,SL,SH,SL,SH,SL,SH,LL,SH,LL,SH,LL,SH,LL,SH,SL,SH,SL,SH,SL,SH
+#define END      DUTY_CYCLE_0
+
+/*
+ * NOTE: These values must be in RAM due to EasyDMA limitations.
+ */
+static uint16_t BLUE_PISTOL_CMD[] = {PREFIX, END};
+
+static nrfx_pwm_t m_pwm = NRFX_PWM_INSTANCE(0);
+static nrf_pwm_sequence_t m_seq = {
+    .values.p_common = NULL,
+    .length          = 0,
+    .repeats         = REFRESH_COUNT_400US,
+    .end_delay       = 0
+};
 
 static void m_button_changed(uint32_t button_state, uint32_t has_changed)
 {
@@ -23,7 +60,6 @@ static void m_button_changed(uint32_t button_state, uint32_t has_changed)
 static void m_configure_gpio(void)
 {
 	int err;
-
 	err = dk_buttons_init(m_button_changed);
 	if (err) {
 		LOG_ERR("Cannot init buttons (err: %d)", err);
@@ -37,34 +73,16 @@ static void m_configure_gpio(void)
 
 static void m_pwm_handler(nrfx_pwm_evt_type_t event_type, void * p_context)
 {
+	// TODO: Wait for the NRFX_PWM_EVT_STOPPED event before allowing next blast.
 	LOG_INF("PWM event: %d", event_type);
 }
 
-static nrfx_pwm_t m_pwm = NRFX_PWM_INSTANCE(0);
-static nrf_pwm_values_common_t m_pwm_values[5];
-
 static nrfx_err_t m_pwm_config(void)
 {
-	nrfx_err_t nrfx_err;
-
-	nrfx_pwm_config_t config = NRFX_PWM_DEFAULT_CONFIG(31,
-												NRFX_PWM_PIN_NOT_USED,
-												NRFX_PWM_PIN_NOT_USED,
-												NRFX_PWM_PIN_NOT_USED);
-
-#define PWM_CLOKC_FREQ   8000000
-#define IR_FREQ          37900
-#define TICKS_PER_PERIOD 211
-
-	m_pwm_values[0] = TICKS_PER_PERIOD / 2;
-	m_pwm_values[1] = TICKS_PER_PERIOD;
-	m_pwm_values[2] = TICKS_PER_PERIOD / 2;
-	m_pwm_values[3] = TICKS_PER_PERIOD / 2;
-	m_pwm_values[4] = TICKS_PER_PERIOD;
-//	m_pwm_values[0] = (TICKS_PER_PERIOD / 2);
-//	m_pwm_values[1] = 0;
-//	m_pwm_values[2] = TICKS_PER_PERIOD;
-
+	nrfx_pwm_config_t config = NRFX_PWM_DEFAULT_CONFIG(PWM_PIN,
+														NRFX_PWM_PIN_NOT_USED,
+														NRFX_PWM_PIN_NOT_USED,
+														NRFX_PWM_PIN_NOT_USED);
 	config.base_clock = NRF_PWM_CLK_8MHz;
 	config.top_value  = TICKS_PER_PERIOD;
 	config.load_mode  = NRF_PWM_LOAD_COMMON;
@@ -74,27 +92,15 @@ static nrfx_err_t m_pwm_config(void)
             nrfx_isr,
             nrfx_pwm_0_irq_handler,
             0);
-
-	nrfx_err = nrfx_pwm_init(&m_pwm, &config, m_pwm_handler, NULL);
-	if (NRFX_SUCCESS != nrfx_err)
-	{
-		return nrfx_err;
-	}
-
-	return nrfx_err;
+	return nrfx_pwm_init(&m_pwm, &config, m_pwm_handler, NULL);
 }
 
-static void m_blast(void)
+static void m_blast(uint16_t *data, uint32_t len)
 {
-    nrf_pwm_sequence_t const seq =
-    {
-        .values.p_common = &m_pwm_values[0],
-        .length              = NRF_PWM_VALUES_LENGTH(m_pwm_values),
-        .repeats             = 14,
-        .end_delay           = 0
-    };
+    m_seq.values.p_common = data;
+    m_seq.length          = len;
     nrfx_pwm_simple_playback(&m_pwm,
-                             &seq,
+                             &m_seq,
                              1,
                              NRFX_PWM_FLAG_STOP);
 }
@@ -104,22 +110,20 @@ void main(void)
 	nrfx_err_t nrfx_err;
 
 	m_configure_gpio();
+
 	nrfx_err = m_pwm_config();
 	if (NRFX_SUCCESS != nrfx_err) {
+		LOG_ERR("PWM config failed: %x", nrfx_err);
 		dk_set_led_on(DK_LED2);
-	}
-
-	for (int i=0; i < 5; i++) {
-		m_blast();
-		k_sleep(K_MSEC(100));
 	}
 
 	LOG_INF("Turret started.");
 
-	while(1) {
+	while (true) {
+		m_blast(&BLUE_PISTOL_CMD[0], NRF_PWM_VALUES_LENGTH(BLUE_PISTOL_CMD));
 		dk_set_led_on(DK_LED1);
-		k_sleep(K_MSEC(100));
+		k_sleep(K_MSEC(50));
  		dk_set_led_off(DK_LED1);
-		k_sleep(K_MSEC(100));
+		k_sleep(K_MSEC(50));
 	}
 }
