fb1f59ffc3cd2466804a202f5dc2c8e49b279ba4
Daniel Veilleux <inductivekickback@gmail.com>
2021-03-10 21:41:56 -0800

    Add blank project

---
+++ samples/sensor/pewpew/src/main.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021 Daniel Veilleux
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <string.h>
+#include <zephyr.h>
+#include <device.h>
+#include <stdio.h>
+#include <sys/__assert.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+void main(void)
+{
+    int ret;
+    const struct device *dev;
+
+    if (IS_ENABLED(CONFIG_LOG_BACKEND_RTT)) {
+        /* Give RTT log time to be flushed before executing tests */
+        k_sleep(K_MSEC(500));
+    }
+
+    dev = device_get_binding("PEW_PEW_RX");
+
+    if (dev == NULL) {
+        LOG_ERR("Failed to get dev binding");
+        return;
+    }
+    LOG_INF("dev is %p, name is %s", dev, dev->name);
+    while (1) {
+    	k_sleep(K_MSEC(5));
+    }
+}
