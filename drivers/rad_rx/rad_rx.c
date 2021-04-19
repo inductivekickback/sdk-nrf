92b407d80c23420e2813688eb5bec2e2d5873500
Daniel Veilleux <inductivekickback@gmail.com>
2021-04-18 22:44:43 -0700

    Fine-tuned msg lib access

--- drivers/rad_rx/rad_rx.c
+++ drivers/rad_rx/rad_rx.c
@@ -8,9 +8,13 @@
  * TODO: Pin changes are serviced by interrupts so no separate thread is needed.
  *         - This means processing needs to be short.
  *         - Measure the elapsed time and then add item to sys queue
- *         - Then process and create another work queue item to execute callback.
+ *         - Then process and create another work queue item to execute callback when necessary.
  *         - But need to reset message index and state from there.
  *       How to synchronize between pin-change interrupt and sys queue processing??????
+ *         - If we wait until the line clears then that is added latency.
+ *         - If we parse as soon as minimum length is achieved then we need to skip invalid messages
+ *         - Once a message is accepted/rejected by all libs then wait for clear.
+ *         - Only try to parse on rising edge.
  */
 
 #define DT_DRV_COMPAT dmv_rad_rx
@@ -22,11 +26,8 @@
 
 #include <logging/log.h>
 
+#include <rad.h>
 #include <drivers/rad_rx.h>
-
-// TODO: These needs to be defined according to which message types are enabled.
-#define MAX_MSG_TIMESTAMPS 40
-#define LINE_CLEAR_MS      5
 
 LOG_MODULE_REGISTER(rad_rx, CONFIG_RAD_RX_LOG_LEVEL);
 
@@ -49,9 +50,20 @@ struct rad_rx_data {
     struct k_timer        timer;
     struct k_work         work;
 
-    uint32_t              message[MAX_MSG_TIMESTAMPS];
+    uint32_t              message[RAD_MSG_MAX_LEN];
+    uint32_t              timestamp;
     atomic_t              index;
     msg_state_t           state;
+
+#if CONFIG_RAD_RX_ACCEPT_RAD
+    parse_state_t         rad_parse_state;
+#endif
+#if CONFIG_RAD_RX_ACCEPT_DYNASTY
+    parse_state_t         dynasty_parse_state;
+#endif
+#if CONFIG_RAD_RX_ACCEPT_LASER_X
+    parse_state_t         laser_x_parse_state;
+#endif
 };
 
 struct rad_rx_cfg {
@@ -66,8 +78,17 @@ static void line_clear_timer_expire(struct k_timer *timer_id)
     //       to preclude a race condition.
     struct rad_rx_data *data = CONTAINER_OF(timer_id, struct rad_rx_data, timer);
 
-    data->state = MSG_STATE_WAIT_FOR_PREAMBLE;
-    data->index = 0;
+    data->state               = MSG_STATE_WAIT_FOR_PREAMBLE;
+    data->index               = 0;
+#if CONFIG_RAD_RX_ACCEPT_RAD
+    data->rad_parse_state     = PARSE_STATE_INCOMPLETE;
+#endif
+#if CONFIG_RAD_RX_ACCEPT_DYNASTY
+    data->dynasty_parse_state = PARSE_STATE_INCOMPLETE;
+#endif
+#if CONFIG_RAD_RX_ACCEPT_LASER_X
+    data->laser_x_parse_state = PARSE_STATE_INCOMPLETE;
+#endif
 }
 
 static void message_decode(struct k_work *item)
@@ -94,7 +115,7 @@ static void message_decode(struct k_work *item)
     //       and start the timer.
 
     data->state = MSG_STATE_WAIT_FOR_LINE_CLEAR;
-    k_timer_start(&data->timer, K_MSEC(LINE_CLEAR_MS), K_NO_WAIT);
+    k_timer_start(&data->timer, K_MSEC(RAD_MSG_LINE_CLEAR_LEN_MS), K_NO_WAIT);
 }
 
 static void input_changed(const struct device *dev, struct gpio_callback *cb_data, uint32_t pins)
@@ -102,12 +123,12 @@ static void input_changed(const struct device *dev, struct gpio_callback *cb_dat
     struct rad_rx_data *data = CONTAINER_OF(cb_data, struct rad_rx_data, cb_data);
 
     if (MSG_STATE_WAIT_FOR_LINE_CLEAR == data->state) {
-        k_timer_start(&data->timer, K_MSEC(LINE_CLEAR_MS), K_NO_WAIT);
+        k_timer_start(&data->timer, K_MSEC(RAD_MSG_LINE_CLEAR_LEN_MS), K_NO_WAIT);
         return;
     }
 
     uint32_t index = atomic_inc(&data->index);
-    if (MAX_MSG_TIMESTAMPS > index) {
+    if (RAD_MSG_MAX_LEN > index) {
         data->message[index] = k_cycle_get_32();
         k_work_submit(&data->work);
     }
--- lib/rad_message_type_rad/rad_message_type_rad.c
+++ lib/rad_message_type_rad/rad_message_type_rad.c
@@ -4,15 +4,35 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+/*
+ * 38KHz carrier
+ * Want a minimum burst length of 10 cycles
+ * Use gap of at least 10 cycles for short ons (10 to 35)
+ * Use gap of 10x for longer ons 
+ * Max number of on bursts is 1500 per second
+ */
+
 #include <zephyr.h>
 #include <sys/util.h>
 #include <logging/log.h>
 
 #include <rad.h>
-	
+
 LOG_MODULE_REGISTER(rad_message_type_rad, CONFIG_RAD_MESSAGE_TYPE_RAD_LOG_LEVEL);
 
-void rad_message_type_rad_init(void)
+parse_state_t rad_message_type_rad_parse(uint32_t *message, uint32_t len)
 {
-	// TODO:
+	// TODO: Compare the preamble so it fails faster?
+
+	if (RAD_MSG_TYPE_RAD_MIN_MSG_LEN > len) {
+		return PARSE_STATE_INCOMPLETE;
+	}
+
+	if (RAD_MSG_TYPE_RAD_MAX_MSG_LEN < len) {
+		return PARSE_STATE_INVALID;
+	}
+
+	// TODO: Actually verify it.
+
+	return PARSE_STATE_VALID;
 }
